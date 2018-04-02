b1fae5aa006ea4d6d78faa64c14ac82fc7dbee4c
George Stavrinos <gstavrinos@iit.demokritos.gr>
2018-04-02 18:59:19 +0300

    Some cool additions. Glade is awesome! And Julia of course! :heart:

--- src/scoreboard.jl
+++ src/scoreboard.jl
@@ -2,7 +2,11 @@
 using Gtk.ShortNames
 
 function main()
-    window = Window("Scoreboard", 640, 480, true, true)
+    glade_file = rsplit(@__FILE__,"/",limit=3)[1] * "/glade_files/main_window.glade"
+    builder = Builder(filename=glade_file)
+    window = builder["applicationwindow1"]
+    setproperty!(window, :title, "Timed Scoreboard :)")
+    showall(window)
 
     # Put your GUI code here
 
